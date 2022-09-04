3729f0adc85bf8b3ec3ce43d26cc815cff1fcef5
Doomdon <2andrusha3@gmail.com>
2022-09-04 18:25:40 +0300

    add task

--- main.py
+++ main.py
@@ -2,11 +2,26 @@ import speech_recognition
 
 
 sr = speech_recognition.Recognizer()
-sr.pause_threshold = 0.9
+sr.pause_threshold = 0.5
 
 def greeting():
 
     return 'Привет, Господин!'
+
+def task():
+    print('Что записать, хозяин?')
+
+    with speech_recognition.Microphone() as mic:
+        sr.adjust_for_ambient_noise(source=mic, duration=0.10)
+        audio = sr.listen(source=mic)
+        query = sr.recognize_google(audio_data=audio, language='ru-RU').lower()
+
+    with open('task.txt', 'a') as file:
+        file.write(f'{query}\n')
+
+    return f'Задача {query} добавлена'
+
+
 
 with speech_recognition.Microphone() as mic:
     sr.adjust_for_ambient_noise(source=mic, duration=0.5)
@@ -15,8 +30,8 @@ with speech_recognition.Microphone() as mic:
 
 if query == 'в махачкалу ебанём':
     print(greeting())
-elif query == 'алло':
-    print('Че надо, хозяин?')
+elif query == 'добавь задачу':
+    print(task())
 elif query == 'серёга петух':
     print('согласен, еще и пидорас))')
 else:
